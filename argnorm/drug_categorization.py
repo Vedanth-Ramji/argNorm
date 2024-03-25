c17e2df413abf061e527f62281073e63b48e7912
Vedanth Ramji <vedanth.ramji@outlook.com>
2024-03-25 21:43:40 +1000

    Testing whole ARO columns. Added integration tests. Removed Python syntax in outputs.

--- argnorm/drug_categorization.py
+++ argnorm/drug_categorization.py
@@ -1,19 +1,24 @@
+# These functions are run in the run function of the BaseNormalizer.
+# After ARO numbers are obtained, these functions can be executed - hence should be independent of which db is used.
+
 import pronto
-from typing import List
+from typing import List, Tuple
 
 # Load the ARO ontology from internet
 ARO = pronto.Ontology.from_obo_library('aro.obo')
 
-def confers_resistance_to(aro_num: str) -> List[str]:
+def confers_resistance_to(aro_num: str) -> List[Tuple]:
     '''
-    Description: Returns a list of the drugs/antibiotics to which a gene confers resistance to.
+    Description: Gets the drugs/antibiotics to which a gene confers resistance to.
+    Only lists the drug class column in the CARD db.
 
     Parameters:
         aro_num (str): ARO number. Needs to be in the form 'ARO:number'.
 
     Returns:
-        drugs_list (list[str]):
-            A list with ARO number of the drugs/antibiotics to which the input gene confers resistance to.
+        drugs_list (list[tuple]):
+            A two-dimensional list where each inner list represents a drug.
+            Each inner list contains the ARO number and name of the drug in that order. [ARO:number, name].
     '''
 
     # Some databases don't provide aro numbers as strings.
@@ -47,18 +52,20 @@ def confers_resistance_to(aro_num: str) -> List[str]:
 
     return sorted(drugs_list)
 
-def drugs_to_drug_classes(drugs_list: List[str]) -> List[str]:
+def drugs_to_drug_classes(drugs_list: List[Tuple]) -> List[Tuple]:
     '''
-    Description: Returns a list of categories of drug classes, e.g. cephem and penam are categorized as beta_lactam antibiotics.
+    Description: Gives a list of categories of drug classes, e.g. cephem and penam are categorized as beta_lactam antibiotics.
 
     Parameters:
-        drugs_list (list[str]):
-            A list with ARO number of the drugs/antibiotics to which a gene confers resistance to.
+        drugs_list (list[tuple]):
+            A two-dimensional list where each inner list represents a drug.
+            Each inner list contains the ARO number and name of the drug in that order. [ARO:number, name].
+            Designed to use the return value of the function 'confers_resistance_to'.
 
     Returns:
         drug_classes (list[str]):
-            A list containing the ARO numbers of the drug classes of each drug given as input to the function.
-            Order of the ARO numbers of the drug classes corresponds to the order in which the drugs were given
+            A list containing the names of the drug classes of each drug given as input to the function.
+            Order of the names of the drug classes corresponds to the order in which the drugs were given
             to the function in the drugs_list.
     '''
     drug_classes = []
